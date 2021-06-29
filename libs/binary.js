468c7211a39ae8a630ae04d575ab3a76133af624
Ariane <pikabooariane@hotmail.fr>
2021-06-29 14:44:38 +0300

    fix/formatting

--- libs/binary.js
+++ libs/binary.js
@@ -5,7 +5,7 @@ exports.GIGABIT_SIZE = 1024**3;
 
 // Get ASCII string from byte subarray
 exports.bytesString = (byteArray, startWord, endWord) => {
-  return String.fromCharCode(...byteArray.slice(startWord * 2, (endWord + 1) * 2))
+  return String.fromCharCode(...byteArray.slice(startWord * 2, (endWord + 1) * 2));
 };
 
 // Convert a byte array into an integer
@@ -13,7 +13,7 @@ exports.byteArrayToLong = (byteArray) => {
   let numericalValue = 0;
   for ( let i = byteArray.length - 1; i >= 0; i--) {
     numericalValue = (numericalValue * BUFFER_SIZE) + byteArray[i];
-  }
+  };
   return numericalValue;
 };
 
@@ -23,6 +23,6 @@ exports.byteToBit = (byte) => {
   for (let i = 7; i >= 0; i--) {
     let bit = byte & (1 << i) ? 1 : 0;
     bitArray.push(bit);
-  }
+  };
   return bitArray;
 };
